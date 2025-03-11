e3db5df785f89b9c73b9d2040a8ee25f3e624fc4
Ann Yun <by.ann.yun@gmail.com>
2025-03-11 15:08:48 -0400

    cleanup

--- arm32/001.s
+++ arm32/001.s
@@ -1,6 +1,11 @@
-@print out a text to the screen
+@Get a user input and echo it back
 
 .global _start
+.equ BUFFER_MAX_LENGTH, 10
+
+.section .bss
+buffer: 	.space 	BUFFER_MAX_LENGTH	@set aside 10 bytes of data 
+input_len:	.space 	1			@max value will be 10, so 1 byte is sufficient
 
 .section .data
 prompt: 	.asciz 	"enter a string to echo\n"
@@ -8,7 +13,7 @@ prompt_len: 	.word 	. - prompt	@automatically calculate the lengh
 
 .section .text
 _start:
-	@ print out a prompt
+	@print out a prompt
 	mov r0, #1 	@fd = STDOUT
 	ldr r1, =prompt
 	ldr r2, =prompt_len
@@ -16,7 +21,7 @@ _start:
 	mov r7, #0x04	@write syscall
 	swi 0
 
-	@ read the input
+	@read the input
 	mov r0, #0 	@fd = STDIN
 	ldr r1, =buffer
 	mov r2, #BUFFER_MAX_LENGTH
@@ -33,15 +38,7 @@ _start:
 	mov r7, #0x04	@write syscall
 	swi 0
 
-	@ exit the program
+	@exit the program
 	mov r0, #13	@a random error code
 	mov r7, #0x1	@exit syscall
 	swi 0
-
-
-
-.section .bss
-.equ BUFFER_MAX_LENGTH, 10
-buffer: 	.space 	BUFFER_MAX_LENGTH	@set aside 10 bytes of data 
-input_len:	.space 	1			@max value will be 10, so 1 byte is sufficient
-
